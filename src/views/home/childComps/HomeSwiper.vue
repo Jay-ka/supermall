52e764d60a7de38adb4c5224a7bf3817a3152bf8
Jay-ka <2521005911@qq.com>
2020-07-30 20:48:36 +0800

    first commit

---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,25 @@
+<template>
+  <swiper>
+    <swiper-item v-for="item in banners">
+      <a :href="item.link">
+        <img :src="item.image" alt="">
+      </a>
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+  import Swiper from "../../components/common/swiper/Swiper";
+  import SwiperItem from "../../components/common/swiper/SwiperItem";
+  export default {
+    name: "HomeSwiper",
+    comments: {
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
